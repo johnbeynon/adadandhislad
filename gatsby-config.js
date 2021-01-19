4c982736a5d28f31dc3a9d30f5fe30ac172fa8f3
John Beynon <jbeynon@salesforce.com>
2021-01-19 10:13:47 +0000

    Updates for ADAHL

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `A Dad and his lad`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
